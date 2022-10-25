e71347ba161102fee92a2f03b561e7819f89e35e
thanatpp <spy.fo3@gmail.com>
2022-10-25 10:44:45 +0700

    Add prettierr

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,3 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
-</script>
-
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
 <style>
   .logo {
     height: 6em;
@@ -43,3 +14,31 @@
     color: #888;
   }
 </style>
+
+<script lang="ts">
+  import svelteLogo from "./assets/svelte.svg"
+  import Counter from "./lib/Counter.svelte"
+</script>
+
+<main>
+  <div>
+    <a href="https://vitejs.dev">
+      <img src="/vite.svg" class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev">
+      <img src="{svelteLogo}" class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Worklog Github Pages</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a>,
+    the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+</main>
